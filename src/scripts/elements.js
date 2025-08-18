d9aadeeaeb388d26fe20728d162025818b7ad7df
Kareem Abdelkarem <km6263458@gmail.com>
2025-08-18 18:12:07 +0300

    DOM selected elements

---
+++ src/scripts/elements.js
@@ -0,0 +1,6 @@
+export const buttonElement = document.querySelector(".button");
+export const searchInput = document.querySelector(".input");
+export const userInputElement = document.querySelector("input[value='users']");
+export const cardsElement = document.querySelector(".cards");
+export const messageElement = document.querySelector(".message");
+export const loadingElement = document.querySelector(".loader");
