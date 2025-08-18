9c959194fed27849fbc15fae8339baf82dcf7a23
Kareem Abdelkarem <km6263458@gmail.com>
2025-08-18 18:13:23 +0300

    feat: add button event listner

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.scss";
+import {
+  buttonElement,
+  searchInput,
+  userInputElement,
+} from "./scripts/elements";
+import { performSearch } from "./scripts/performSearch";
+
+buttonElement.addEventListener("click", (e) => {
+  e.preventDefault();
+  performSearch(searchInput.value, userInputElement.checked);
+});
